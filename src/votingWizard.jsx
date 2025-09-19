e4b5b952f0ca1a63e82a1c8b658bb341881c1353
Charles Lamb <charles.lamb.dev@gmail.com>
2025-09-18 23:53:39 -0400

    added the artist name to the vote card

--- src/votingWizard.jsx
+++ src/votingWizard.jsx
@@ -47,7 +47,8 @@ const VotingWizard = ({ show, onClose, onVoteSuccess, nominee, filters }) => {
       case 1:
         return (
           <div className="step-content">
-            <h2>Confirm Your Vote</h2>
+            <h2>Confirm Your Vote For:</h2>
+            <h3 className='nominee-name'>{selectedNominee.name}</h3>
             <p className="wizard-intro">Please review your selections below. You can make changes before you confirm.</p>
             
             <div className="filter-selection-grid">
