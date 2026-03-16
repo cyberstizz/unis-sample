621a29ef4a8c1df9b1e79ec14e3502eba599c4d1
Charles Lamb <charles.lamb.dev@gmail.com>
2026-03-15 21:05:15 -0400

    more styling changs

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import Header from './header'; 
+import Header from './header';
 import Footer from './footer';
-import './layout.scss'; 
+import './layout.scss';
 
 const Layout = ({ children, backgroundImage }) => {
   return (
-    <div className="layout-container" style={{ '--background-image': `url(${backgroundImage})` }}>
+    <div
+      className="layout-container"
+      style={{
+        '--background-image': `url(${backgroundImage})`
+      }}
+    >
       <Header />
+
       <main className="layout-content">
         {children}
       </main>
-      <Footer />  
+
+      <Footer />
     </div>
   );
 };
