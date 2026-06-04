5642df775b6faf9881477f5108693eb8b6186eef
Charles Lamb <charles.lamb.dev@gmail.com>
2026-06-04 01:20:08 -0400

    revamped the songnotification to show a waveform instead of eq bars

--- src/songNotification.jsx
+++ src/songNotification.jsx
@@ -24,13 +24,11 @@ const SongNotification = () => {
     <div className="song-notification">
       <div className="card">
 
-        {/* Ambient blurred background sourced from artwork */}
         <div
           className="ambient-bg"
           style={{ backgroundImage: `url(${artworkSrc})` }}
         />
 
-        {/* Foreground content */}
         <div className="glass-content">
           <img
             src={artworkSrc}
@@ -46,12 +44,123 @@ const SongNotification = () => {
             </p>
 
             <div className="notification-waveform">
-              {[...Array(18)].map((_, i) => (
-                <span
-                  key={i}
-                  className={`wave-bar wave-bar-${i % 6}`}
-                />
-              ))}
+              <div className="wave-scroll">
+
+                <svg
+                  className="wave-svg"
+                  viewBox="0 0 600 40"
+                  preserveAspectRatio="none"
+                >
+                  <path
+                    d="
+                      M0 20
+                      L20 20
+                      L30 8
+                      L40 32
+                      L50 20
+                      L70 20
+                      L80 10
+                      L90 30
+                      L100 20
+                      L120 20
+                      L130 5
+                      L140 35
+                      L150 20
+                      L170 20
+                      L180 12
+                      L190 28
+                      L200 20
+                      L220 20
+                      L230 7
+                      L240 33
+                      L250 20
+                      L270 20
+                      L280 10
+                      L290 30
+                      L300 20
+                      L320 20
+                      L330 6
+                      L340 34
+                      L350 20
+                      L370 20
+                      L380 11
+                      L390 29
+                      L400 20
+                      L420 20
+                      L430 8
+                      L440 32
+                      L450 20
+                      L470 20
+                      L480 10
+                      L490 30
+                      L500 20
+                      L520 20
+                      L530 6
+                      L540 34
+                      L550 20
+                      L600 20
+                    "
+                  />
+                </svg>
+
+                <svg
+                  className="wave-svg"
+                  viewBox="0 0 600 40"
+                  preserveAspectRatio="none"
+                >
+                  <path
+                    d="
+                      M0 20
+                      L20 20
+                      L30 8
+                      L40 32
+                      L50 20
+                      L70 20
+                      L80 10
+                      L90 30
+                      L100 20
+                      L120 20
+                      L130 5
+                      L140 35
+                      L150 20
+                      L170 20
+                      L180 12
+                      L190 28
+                      L200 20
+                      L220 20
+                      L230 7
+                      L240 33
+                      L250 20
+                      L270 20
+                      L280 10
+                      L290 30
+                      L300 20
+                      L320 20
+                      L330 6
+                      L340 34
+                      L350 20
+                      L370 20
+                      L380 11
+                      L390 29
+                      L400 20
+                      L420 20
+                      L430 8
+                      L440 32
+                      L450 20
+                      L470 20
+                      L480 10
+                      L490 30
+                      L500 20
+                      L520 20
+                      L530 6
+                      L540 34
+                      L550 20
+                      L600 20
+                    "
+                  />
+                </svg>
+
+              </div>
             </div>
           </div>
 
